c60a6dd6a02ebe10236f58e1be5aa8bd8ba8972c
Aman Patel <patel.26@iitj.ac.in>
2025-01-22 20:30:35 +0530

    deleted nodejs backend

--- medSAM/backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import { config } from './config/config';
-import { logger } from './utils/logger';
-
-app.listen(config.port, () => {
-  logger.info(`Server running on http://localhost:${config.port}`);
-});
